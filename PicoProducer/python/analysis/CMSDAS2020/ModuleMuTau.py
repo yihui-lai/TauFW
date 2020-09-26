9348ad7026afdc78f2b858d14ec1f0fe7f36799a
Yihui Lai <yihuilai@foxmail.com>
2020-09-26 19:21:20 +0200

    complete tasks

--- PicoProducer/python/analysis/CMSDAS2020/ModuleMuTau.py
+++ PicoProducer/python/analysis/CMSDAS2020/ModuleMuTau.py
@@ -2,6 +2,7 @@
 # Description: Simple module to pre-select mutau events
 from ROOT import TFile, TTree, TH1D
 from ROOT import Math
+import math
 import numpy as np
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object
@@ -66,6 +67,37 @@ class ModuleMuTau(Module):
     self.decayMode_2 = np.zeros(1,dtype='i')
     self.m_vis       = np.zeros(1,dtype='f')
     self.genWeight   = np.zeros(1,dtype='f')
+    self.jetnumber = np.zeros(1,dtype='i')
+    self.jet_leading_pt = np.zeros(1,dtype='f')
+    self.jet_leading_eta = np.zeros(1,dtype='f')
+    self.jet_sub_pt = np.zeros(1,dtype='f')
+    self.jet_sub_eta = np.zeros(1,dtype='f')
+
+    self.bjetnumber = np.zeros(1,dtype='i')
+    self.bjet_leading_pt = np.zeros(1,dtype='f')
+    self.bjet_leading_eta = np.zeros(1,dtype='f')
+    self.bjet_sub_pt = np.zeros(1,dtype='f')
+    self.bjet_sub_eta = np.zeros(1,dtype='f')
+    
+    self.metpt = np.zeros(1,dtype='f')
+    self.metphi = np.zeros(1,dtype='f')
+    self.metsumEt = np.zeros(1,dtype='f')
+
+    self.puppimetpt = np.zeros(1,dtype='f')
+    self.puppimetphi = np.zeros(1,dtype='f')
+    self.puppimetsumEt = np.zeros(1,dtype='f')
+
+    self.z_vis_pt = np.zeros(1,dtype='f')
+    self.z_full_pt = np.zeros(1,dtype='f')
+    self.dphi = np.zeros(1,dtype='f')
+    self.mt = np.zeros(1,dtype='f')
+    self.miss_Dzeta = np.zeros(1,dtype='f')
+    self.vis_Dzeta = np.zeros(1,dtype='f')
+    self.deltaR_mu_tau  = np.zeros(1,dtype='f')
+    self.pu_rho         = np.zeros(1,dtype='f')
+    self.npvs           = np.zeros(1,dtype='i')
+    self.gennpus = np.zeros(1,dtype='i')
+
     self.tree.Branch('pt_1',         self.pt_1,        'pt_1/F'       )
     self.tree.Branch('eta_1',        self.eta_1,       'eta_1/F'      )
     self.tree.Branch('q_1',          self.q_1,         'q_1/I'        )
@@ -84,7 +116,35 @@ class ModuleMuTau(Module):
     self.tree.Branch('decayMode_2',  self.decayMode_2, 'decayMode_2/I')
     self.tree.Branch('m_vis',        self.m_vis, 'm_vis/F'            )
     self.tree.Branch('genWeight',    self.genWeight,   'genWeight/F'  )
-  
+    self.tree.Branch('jetnumber',          self.jetnumber,         'jetnumber/I'        )
+    self.tree.Branch('jet_leading_pt',         self.jet_leading_pt,        'jet_leading_pt/F'       )
+    self.tree.Branch('jet_leading_eta',         self.jet_leading_eta,        'jet_leading_eta/F'       )
+    self.tree.Branch('jet_sub_pt',         self.jet_sub_pt,        'jet_sub_pt/F'       )
+    self.tree.Branch('jet_sub_eta',         self.jet_sub_eta,        'jet_sub_eta/F'       )
+
+    self.tree.Branch('bjetnumber',          self.bjetnumber,         'bjetnumber/I'        )
+    self.tree.Branch('bjet_leading_pt',         self.bjet_leading_pt,        'bjet_leading_pt/F'       )
+    self.tree.Branch('bjet_leading_eta',         self.bjet_leading_eta,        'bjet_leading_eta/F'       )
+    self.tree.Branch('bjet_sub_pt',         self.bjet_sub_pt,        'bjet_sub_pt/F'       )
+    self.tree.Branch('bjet_sub_eta',         self.bjet_sub_eta,        'bjet_sub_eta/F'       )
+    self.tree.Branch('metpt',         self.metpt,  'metpt/F'             )
+    self.tree.Branch('metphi',         self.metphi,  'metphi/F'             )
+    self.tree.Branch('metsumEt',         self.metsumEt,  'metsumEt/F'             )
+
+    self.tree.Branch('puppimetpt',         self.puppimetpt,  'puppimetpt/F'             )
+    self.tree.Branch('puppimetphi',         self.puppimetphi,  'puppimetphi/F'             )
+    self.tree.Branch('puppimetsumEt',         self.puppimetsumEt,  'puppimetsumEt/F'             )
+    self.tree.Branch('z_vis_pt',         self.z_vis_pt,  'z_vis_pt/F'             )
+    self.tree.Branch('z_full_pt',         self.z_full_pt,  'z_full_pt/F'             )
+    self.tree.Branch('dphi',         self.dphi,  'dphi/F'             )
+    self.tree.Branch('mt',         self.mt,  'mt/F'             )
+    self.tree.Branch('miss_Dzeta',         self.miss_Dzeta,  'miss_Dzeta/F'             )
+    self.tree.Branch('vis_Dzeta',         self.vis_Dzeta,  'vis_Dzeta/F'             )
+    self.tree.Branch('deltaR_mu_tau',         self.deltaR_mu_tau,  'deltaR_mu_tau/F'             )
+    self.tree.Branch('pu_rho',         self.pu_rho,  'pu_rho/F'             )
+    self.tree.Branch('npvs',         self.npvs,  'npvs/I'             )
+    self.tree.Branch('gennpus',         self.gennpus,  'gennpus/I'             )
+
   def endJob(self):
     """Wrap up after running on all events and files"""
     self.outfile.Write()
@@ -109,7 +169,8 @@ class ModuleMuTau(Module):
     for muon in Collection(event,'Muon'):
       good_muon = muon.mediumId and muon.pfRelIso04_all < 0.5 and abs(muon.eta) < 2.5
       signal_muon = good_muon and muon.pt > 28.0
-      veto_muon   = False # TODO section 4: introduce a veto muon selection here
+      veto_muon   = good_muon and muon.pt > 15.0
+#      veto_muon   = False # TODO section 4: introduce a veto muon selection here
       if signal_muon:
         muons.append(muon)
       if veto_muon: # CAUTION: that's NOT an elif here and intended in that way!
@@ -118,6 +179,7 @@ class ModuleMuTau(Module):
     if len(muons) == 0: return False
     self.cutflow.Fill(self.cut_muon)
     # TODO section 4: What should be the requirement to veto events with additional muons?
+    if len(veto_muons) > len(muons): return False
     self.cutflow.Fill(self.cut_muon_veto)
     
     # SELECT TAU
@@ -136,7 +198,8 @@ class ModuleMuTau(Module):
     # and a custom isolation cut on PF based isolation using all PF candidates.
     electrons = []
     for electron in Collection(event,'Electron'):
-      veto_electron = False # TODO section 4: introduce a veto electron selection here
+#      veto_electron = False # TODO section 4: introduce a veto electron selection here
+      veto_electron = electron.pt > 15.0 and electron.mvaFall17V2noIso_WPL and  electron.pfRelIso03_all < 0.5
       if veto_electron:
         electrons.append(electron)
     if len(electrons) > 0: return False
@@ -158,12 +221,69 @@ class ModuleMuTau(Module):
     # The collected jets are furthermore not allowed to overlap with the signal muon and signal tau in deltaR, so selected them to have deltaR >= 0.5 w.r.t. the signal muon and signal tau.
     # Then, select for this collection "usual" jets, which have pt > 30 in addition, count their number, and store pt & eta of the leading and subleading jet.
     # For b-tagged jets, require additionally DeepFlavour b+bb+lepb tag with medium WP and |eta| < 2.5, count their number, and store pt & eta of the leading and subleading b-tagged jet.
+    jets = []
+    bjets = []
+    self.jet_leading_pt[0]=-99
+    self.jet_leading_eta[0]=-99
+    self.jet_sub_pt[0]=-99
+    self.jet_sub_eta[0]=-99
+    self.bjet_leading_pt[0]=-99
+    self.bjet_leading_eta[0]=-99
+    self.bjet_sub_pt[0]=-99
+    self.bjet_sub_eta[0]=-99
+    for jet in Collection(event,'Jet'):
+      veto_jet = jet.pt > 20.0 and abs(jet.eta) < 4.7 and jet.puId>0 and jet.jetId>1
+      if veto_jet and jet.DeltaR(muon)>=0.5 and jet.DeltaR(tau)>=0.5:
+        veto_basic_jet = jet.pt > 30.0 
+        if veto_basic_jet:
+          jets.append(jet)
+#          self.jetnumber[0]+=1
+#          if jet.pt > self.jet_leading_pt[0] :
+#            self.jet_leading_pt[0]=jet.pt
+#            self.jet_leading_eta[0]=jet.eta
+#          if jet.pt > self.jet_sub_pt[0] and jet.pt <= self.jet_leading_pt[0] and self.jetnumber[0]>1:
+#            self.jet_sub_pt[0]=jet.pt
+#            self.jet_sub_eta[0]=jet.eta
+        veto_bjet = jet.btagDeepFlavB > 0.2770 and abs(jet.eta) < 2.5
+        if veto_bjet:
+          bjets.append(jet)
+#          self.bjetnumber[0]+=1
+#          if jet.pt > self.bjet_leading_pt[0] :
+#            self.bjet_leading_pt[0]=jet.pt
+#            self.bjet_leading_eta[0]=jet.eta
+#          if jet.pt > self.bjet_sub_pt[0] and jet.pt <= self.bjet_leading_pt[0] and self.bjetnumber[0]>1:
+#            self.bjet_sub_pt[0]=jet.pt
+#            self.bjet_sub_eta[0]=jet.eta
+    self.jetnumber[0]=len(jets)
+    self.bjetnumber[0]=len(bjets)
+    if self.jetnumber[0]>0:
+      jet = max(jets,key=lambda p: p.pt)
+      self.jet_leading_pt[0]=jet.pt
+      self.jet_leading_eta[0]=jet.eta
+    if self.jetnumber[0]>1:
+      new_jets = set(jets)
+      new_jets.remove(max(jets,key=lambda p: p.pt)) 
+      jet = max(new_jets,key=lambda p: p.pt)
+      self.jet_sub_pt[0]=jet.pt
+      self.jet_sub_eta[0]=jet.eta
+    if self.bjetnumber[0]>0:
+      bjet = max(bjets,key=lambda p: p.pt)
+      self.bjet_leading_pt[0]=bjet.pt
+      self.bjet_leading_eta[0]=bjet.eta
+
 
     # CHOOSE MET definition
     # TODO section 4: compare the PuppiMET and (PF-based) MET in terms of mean, resolution and data/expectation agreement of their own distributions and of related quantities
     # and choose one of them for the refinement of Z to tautau selection.
     puppimet = Met(event, 'PuppiMET')
     met = Met(event, 'MET')
+
+    self.metpt[0] = met.pt
+    self.metphi[0] = met.phi
+    self.metsumEt[0] = met.sumEt
+    self.puppimetpt[0] = puppimet.pt
+    self.puppimetphi[0] = puppimet.phi
+    self.puppimetsumEt[0] = puppimet.sumEt
     
     # SAVE VARIABLES
     # TODO section 4: extend the variable list with more quantities (also high level ones). Compute at least:
@@ -191,12 +311,32 @@ class ModuleMuTau(Module):
     self.decayMode_2[0] = tau.decayMode
     self.m_vis[0]       = (muon.p4()+tau.p4()).M()
 
+######### add high level variables #########
+    self.z_vis_pt[0]    = (muon.p4()+tau.p4()).Pt()
+    self.z_full_pt[0]   = (puppimet.p4()+muon.p4()+tau.p4()).Pt()
+    self.dphi[0]        = muon.phi - puppimet.phi 
+    if self.dphi[0] < -1*math.pi:
+        self.dphi[0]+=2*math.pi
+    elif self.dphi[0] > math.pi:
+        self.dphi[0]-=2*math.pi
+    #it seems doesn't matter, cos(phi+/-2pi)=cos(phi)
+    self.mt[0]          = math.sqrt( 2*muon.pt*puppimet.sumEt*(1 - math.cos(self.dphi[0])) ) 
+    bisec = (muon.phi + tau.phi)/2.0
+    self.miss_Dzeta[0]  = puppimet.pt * math.cos(bisec - puppimet.phi)
+    self.vis_Dzeta[0]   = muon.pt*math.cos(bisec - muon.phi) + tau.pt*math.cos(bisec - muon.phi)
+    self.deltaR_mu_tau[0]  = muon.DeltaR(tau)
+    self.pu_rho[0]         = event.fixedGridRhoFastjetAll
+    self.npvs[0]           = event.PV_npvs
+
+
+
     if self.ismc:
       self.genmatch_1[0]  = muon.genPartFlav # in case of muons: 1 == prompt muon, 15 == muon from tau decay, also other values available for muons from jets
       self.genmatch_2[0]  = tau.genPartFlav # in case of taus: 0 == unmatched (corresponds then to jet),
                                             #                  1 == prompt electron, 2 == prompt muon, 3 == electron from tau decay,
                                             #                  4 == muon from tau decay, 5 == hadronic tau decay
-      self.genWeight[0] = event.genWeight
+      self.genWeight[0]   = event.genWeight
+      self.gennpus[0]        = event.Pileup_nPU
     self.tree.Fill()
     
     return True
