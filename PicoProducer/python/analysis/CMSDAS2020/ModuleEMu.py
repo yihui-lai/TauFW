e7f25527072f59427457fd6dfad0333e4b0348be
Yihui Lai <yihui.lai@cern.ch>
2020-09-28 11:35:54 -0400

    update EMu

--- PicoProducer/python/analysis/CMSDAS2020/ModuleEMu.py
+++ PicoProducer/python/analysis/CMSDAS2020/ModuleEMu.py
@@ -6,7 +6,7 @@ import math
 import numpy as np
 from PhysicsTools.NanoAODTools.postprocessing.framework.eventloop import Module
 from PhysicsTools.NanoAODTools.postprocessing.framework.datamodel import Collection, Object
-from TauFW.PicoProducer.analysis.utils import LeptonPair, idIso, matchtaujet
+from TauFW.PicoProducer.analysis.utils import LeptonPair, idIso, matchtaujet. deltaPhi
 
 # Inspired by 'Object' class from NanoAODTools.
 # Convenient to do so to be able to add MET as 4-momentum to other physics objects using p4()
@@ -161,73 +161,57 @@ class ModuleEMu(Module):
     if not event.HLT_IsoMu24 or event.HLT_IsoMu27: return False
     self.cutflow.Fill(self.cut_trig)
     
-    # SELECT MUON
-    muons = [ ]
-    # TODO section 4: extend with a veto of additional muons. Veto muons should have the same quality selection as signal muons (or even looser),
-    # but with a lower pt cut, e.g. muon.pt > 15.0
-    veto_muons = [ ]
 
-    for muon in Collection(event,'Muon'):
-      good_muon = muon.mediumId and muon.pfRelIso04_all < 0.5 and abs(muon.eta) < 2.4
-      signal_muon = good_muon and muon.pt > 25.0 and muon.dz<0.2 and muon.dxy<0.045
-      veto_muon   = good_muon and muon.pt > 15.0
-#      veto_muon   = False # TODO section 4: introduce a veto muon selection here
-      if signal_muon:
-        muons.append(muon)
-      if veto_muon: # CAUTION: that's NOT an elif here and intended in that way!
-        veto_muons.append(muon)
-     
-    if len(muons) == 0: return False
-    self.cutflow.Fill(self.cut_muon)
-    # TODO section 4: What should be the requirement to veto events with additional muons?
-    if len(veto_muons) > len(muons): return False
-    self.cutflow.Fill(self.cut_muon_veto)
     
-    # SELECT TAU
-    # TODO section 6: Which decay modes of a tau should be considered for an analysis? Extend tau selection accordingly
-#    taus = [ ]
-#    for tau in Collection(event,'Tau'):
-#      good_tau = tau.pt > 18.0 and tau.idDeepTau2017v2p1VSe >= 1 and tau.idDeepTau2017v2p1VSmu >= 1 and tau.idDeepTau2017v2p1VSjet >= 1
-#      if good_tau:
-#        taus.append(tau)
-#    if len(taus)<1: return False
-#    self.cutflow.Fill(self.cut_electron)
-
-    # SELECT ELECTRONS FOR VETO
-    # TODO section 4: extend the selection of veto electrons: pt > 15.0,
-    # with loose WP of the mva based ID (Fall17 training without isolation),
-    # and a custom isolation cut on PF based isolation using all PF candidates.
-    electrons = []
-    veto_electrons = []
+    ##### ELECTRON ###################################
+    electrons = [ ]
+    veto_electrons = [ ]
     for electron in Collection(event,'Electron'):
-      good_electron = (electron.mvaFall17V2noIso_WP90 or electron.mvaFall17V2noIso_WP90) and electron.pfRelIso03_all < 0.5 and abs(electron.eta) < 2.3
-      signal_electron = good_electron and electron.pt > 15.0 and electron.dz<0.2 and electron.dxy<0.045
-      veto_electron   = good_electron and electron.pt > 10.0
+      good_electron = (electron.mvaFall17V2noIso_WP90 or electron.mvaFall17V2noIso_WP90) and electron.pfRelIso03_all < 0.5 and abs(electron.eta) < 2.3 and electron.convVeto and electron.lostHits<=1 and electron.dz<0.2 and electron.dxy<0.045
+      signal_electron = good_electron and electron.pt > 15.0 
+      veto_electron   = good_electron and electron.pt > 10.0 and electron.pt <= 15.0 
       if signal_electron:
         electrons.append(electron)
       if veto_electron:
         veto_electrons.append(electron)
     if len(electrons) == 0: return False
     self.cutflow.Fill(self.cut_electron) 
-    if len(veto_electrons) > len(electrons): return False
-    self.cutflow.Fill(self.cut_electron_veto)
+    if len(veto_electrons) > 0: return False
+    self.out.cutflow.fill(self.cut_electron_veto)
     
-    # PAIR
-    # TODO section 4 (optional): the mutau pair is constructed from a muon with highest pt and a tau with highest pt.
-    # However, there is also the possibility to select the mutau pair according to the isolation.
-    # If you like, you could try to implement mutau pair building algorithm, following the instructions on
-    # https://twiki.cern.ch/twiki/bin/view/CMS/HiggsToTauTauWorking2017#Pair_Selection_Algorithm, but using the latest isolation quantities/discriminators
-    muon = max(muons,key=lambda p: p.pt)
-    electron  = max(electrons,key=lambda p: p.pt)
-    if muon.DeltaR(electron)<0.5: return False
-    self.cutflow.Fill(self.cut_pair)
+    ##### MUON #######################################
+    muons = [ ]
+    veto_muons = [ ]
+    for muon in Collection(event,'Muon'):
+      good_muon = muon.mediumId and muon.pfRelIso04_all < 0.5 and abs(muon.eta) < 2.4 and and muon.dz<0.2 and muon.dxy<0.045
+      signal_muon = good_muon and muon.pt > 25.0 
+      veto_muon   = good_muon and muon.pt > 15.0 and muon.pt <= 25.0 
+      if signal_muon:
+        muons.append(muon)
+      if veto_muon: 
+        veto_muons.append(muon)
+    if len(muons)==0: return False
+    self.out.cutflow.fill(self.cut_muon)
+    if len(veto_muons) > 0: return False
+    self.cutflow.Fill(self.cut_muon_veto)
+    
+    ##### eMu PAIR #################################
+    dileps = [ ]
+    for electron in electrons:
+      for muon in muons:
+        if muon.DeltaR(electron)<0.5: continue
+        ltau = LeptonPair(electron,electron.pfRelIso03_all,muon,muon.pfRelIso04_all)
+        dileps.append(ltau)
+    if len(dileps)==0:
+      return False
+    electron, muon = max(dileps).pair
+    #electron.tlv   = electron.p4()
+    #muon.tlv       = muon.p4()
+    self.out.cutflow.fill(self.cut_pair)
+    
+
 
     # SELECT Jets
-    # TODO section 4: Jets are not used directly in our analysis, but it can be good to have a look at least the number of jets (and b-tagged jets) of your selection.
-    # Therefore, collect at first jets with pt > 20, |eta| < 4.7, passing loose WP of Pileup ID, and tight WP for jetID.
-    # The collected jets are furthermore not allowed to overlap with the signal muon and signal tau in deltaR, so selected them to have deltaR >= 0.5 w.r.t. the signal muon and signal tau.
-    # Then, select for this collection "usual" jets, which have pt > 30 in addition, count their number, and store pt & eta of the leading and subleading jet.
-    # For b-tagged jets, require additionally DeepFlavour b+bb+lepb tag with medium WP and |eta| < 2.5, count their number, and store pt & eta of the leading and subleading b-tagged jet.
     jets = []
     bjets = []
     self.jet_leading_pt[0]=-99
@@ -284,40 +268,21 @@ class ModuleEMu(Module):
     self.puppimetsumEt[0] = puppimet.sumEt
     
     # SAVE VARIABLES
-    # TODO section 4: extend the variable list with more quantities (also high level ones). Compute at least:
-    # - visible pt of the Z boson candidate
-    # - best-estimate for pt of Z boson candidate (now including contribution form neutrinos)
-    # - transverse mass of the system composed from the muon and MET vectors. Definition can be found in doi:10.1140/epjc/s10052-018-6146-9.
-    #   Caution: use ROOT DeltaPhi for difference in phi and check that deltaPhi is between -pi and pi.Have a look at transverse mass with both versions of MET
-    # - Dzeta. Definition can be found in doi:10.1140/epjc/s10052-018-6146-9. Have a look at the variable with both versions of MET
-    # - Separation in DeltaR between muon and tau candidate
-    # - global event quantities like the proper definition of pileup density rho, number of reconstructed vertices,
-    # - in case of MC: number of true (!!!) pileup interactions
     self.pt_1[0]        = muon.pt
     self.eta_1[0]       = muon.eta
     self.q_1[0]         = muon.charge
     self.id_1[0]        = muon.mediumId
     self.iso_1[0]       = muon.pfRelIso04_all # keep in mind: the SMALLER the value, the more the muon is isolated
-    self.decayMode_1[0] = self.default_int # not needed for a muon
     self.pt_2[0]        = electron.pt
     self.eta_2[0]       = electron.eta
     self.q_2[0]         = electron.charge
-#    self.id_2[0]        = electron.idDeepTau2017v2p1VSjet
-#    self.anti_e_2[0]    = electron.idDeepTau2017v2p1VSe
-#    self.anti_mu_2[0]   = electron.idDeepTau2017v2p1VSmu
     self.iso_2[0]       = electron.pfRelIso03_all # keep in mind: the HIGHER the value of the discriminator, the more the tau is isolated
-#    self.decayMode_2[0] = electron.decayMode
     self.m_vis[0]       = (muon.p4()+electron.p4()).M()
 
 ######### add high level variables #########
     self.z_vis_pt[0]    = (muon.p4()+electron.p4()).Pt()
     self.z_full_pt[0]   = (puppimet.p4()+muon.p4()+electron.p4()).Pt()
-    self.dphi[0]        = muon.phi - puppimet.phi 
-    if self.dphi[0] < -1*math.pi:
-        self.dphi[0]+=2*math.pi
-    elif self.dphi[0] > math.pi:
-        self.dphi[0]-=2*math.pi
-    #it seems doesn't matter, cos(phi+/-2pi)=cos(phi)
+    self.dphi[0]        = deltaPhi(muon.phi , puppimet.phi )
     self.mt[0]          = math.sqrt( 2*muon.pt*puppimet.sumEt*(1 - math.cos(self.dphi[0])) ) 
     bisec = (muon.phi + electron.phi)/2.0
     self.miss_Dzeta[0]  = puppimet.pt * math.cos(bisec - puppimet.phi)
@@ -330,9 +295,7 @@ class ModuleEMu(Module):
 
     if self.ismc:
       self.genmatch_1[0]  = muon.genPartFlav # in case of muons: 1 == prompt muon, 15 == muon from tau decay, also other values available for muons from jets
-      self.genmatch_2[0]  = electron.genPartFlav # in case of taus: 0 == unmatched (corresponds then to jet),
-                                            #                  1 == prompt electron, 2 == prompt muon, 3 == electron from tau decay,
-                                            #                  4 == muon from tau decay, 5 == hadronic tau decay
+      self.genmatch_2[0]  = electron.genPartFlav #Flavour of genParticle for MC matching to status==1 electrons or photons: 1 = prompt electron (including gamma*->mu mu), 15 = electron from prompt tau, 22 = prompt photon (likely conversion), 5 = electron from b, 4 = electron from c, 3 = electron from light or unknown, 0 = unmatched
       self.genWeight[0]   = event.genWeight
       self.gennpus[0]        = event.Pileup_nPU
     self.tree.Fill()
